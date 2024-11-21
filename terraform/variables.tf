54efe041230456b5ae7ec320f875f5a215bcbef3
Hoan Tran <xhoantran@gmail.com>
2024-11-21 13:23:31 -0500

    Remove unneed variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
-variable "TF_VAR_AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-}
-
-variable "TF_VAR_AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-}
-
 variable "aws_region" {
   default     = "us-east-1"
   description = "The AWS region to deploy to"
