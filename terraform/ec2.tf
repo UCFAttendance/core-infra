a505843e016fa4ad9b61106dbadb6d585ee9548a
Hoan Tran <xhoantran@gmail.com>
2024-11-21 13:16:14 -0500

    Update sg name

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "attendance_ec2_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.attendance_alb_sg.id]
+    security_groups = [aws_security_group.lb_sg.id]
   }
 
   ingress {
