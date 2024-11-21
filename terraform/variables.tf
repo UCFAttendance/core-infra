d7d7b1845b5f621f50d22801f56ddf6074a5dc60
Hoan Tran <xhoantran@gmail.com>
2024-11-21 12:59:02 -0500

    Point ALB to EC2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "The AWS region to deploy to"
 }
+
+variable "certificate_arn" {
+  default     = "arn:aws:acm:us-east-1:354918382277:certificate/7489e6b1-9248-4bda-a547-2711eb7c3f8f"
+  description = "The ARN of the SSL certificate to use for HTTPS"
+}
