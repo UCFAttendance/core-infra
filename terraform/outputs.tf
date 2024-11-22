5e6b0751ae5b7ac58dbf5ca8d0902e11a3d9abcf
Hoan Tran <xhoantran@gmail.com>
2024-11-22 10:04:57 -0500

    Remove ec2 init ecr

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
-output "attendance-vpc-id" {
+output "vpc-id" {
   value = aws_vpc.attendance_vpc.id
+}
+
+output "public-subnet-ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private-subnet-ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "repository-arn" {
+  value = aws_ecr_repository.attendance.arn
 }
