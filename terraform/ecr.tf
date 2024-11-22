f398773ae7fd96f18a893585cd5f692259054dd2
Hoan Tran <xhoantran@gmail.com>
2024-11-22 13:24:39 -0500

    Remove ECR lifecycle

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,27 +1,3 @@
 resource "aws_ecr_repository" "attendance_backend" {
   name = "attendance/backend"
 }
-
-resource "aws_ecr_lifecycle_policy" "backend" {
-  repository = aws_ecr_repository.attendance_backend.name
-
-  policy = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Keep last 7 images",
-      "selection": {
-        "tagStatus": "tagged",
-        "tagPrefixList": ["v"],
-        "countType": "imageCountMoreThan",
-        "countNumber": 7
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-} 
-EOF
-}
