bb2bd7d4b9179e89cd88af70518aee2c8750b80b
Hoan Tran <xhoantran@gmail.com>
2024-11-22 14:36:44 -0500

    Update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,16 +10,16 @@ output "private-subnet-ids" {
   value = aws_subnet.private[*].id
 }
 
-output "rds-endpoint" {
-  value = aws_db_instance.ucf_attendance_db.endpoint
+output "rds-indentifier" {
+  value = aws_db_instance.ucf_attendance_db.identifier
 }
 
-output "alb-id" {
-  value = aws_lb.app_load_balancer.id
+output "alb-arn" {
+  value = aws_lb.app_load_balancer.arn
 }
 
-output "ecs-cluster-id" {
-  value = aws_ecs_cluster.ecs_cluster.id
+output "ecs-cluster-name" {
+  value = aws_ecs_cluster.ecs_cluster.name
 }
 
 output "repository-arn" {
